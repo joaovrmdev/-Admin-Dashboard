40b3dd13d558dbe3d4d251da697d1fa285c2c465
JohnVRMDev <joao.mata1111@gmail.com>
2022-03-13 11:18:56 -0300

    index.js + tema dark
    Fazendo o menu aparecer quando, há o click. Uso do javascrip para o menu e para alternar temas.

---
+++ index.js
@@ -0,0 +1,23 @@
+const sideMenu = document.querySelector("aside");
+const menuBtn = document.querySelector("#menu-btn");
+const closeBtn = document.querySelector("#close-btn");
+const themeToggler = document.querySelector(".theme-toggler");
+themeToggler.querySelector('span').classList.toggle('active');
+
+//Show sidebar
+menuBtn.addEventListener('click', () =>{
+    sideMenu.style.display= 'block';
+})
+
+//close sidebar
+closeBtn.addEventListener('click', () => {
+        sideMenu.style.display = 'active';
+})
+
+//change theme
+themeToggler.addEventListener('click',() => {
+    document.body.classList.toggle('dark-theme-variables');
+
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
